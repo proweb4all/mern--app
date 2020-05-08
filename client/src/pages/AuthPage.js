549aa1e02db945bbaa7ad3c7eec5d6d9362d617a
proweb4all <proweb4all@narod.ru>
2020-05-08 14:37:55 +0300

    1:05:31 React-routes

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export const AuthPage = () => {
+  return (
+    <div>
+      <h1>Auth page</h1>
+    </div>
+  )
+}
---
+++ client/src/pages/DetailPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export const DetailPage = () => {
+  return (
+    <div>
+      <h1>Detail page</h1>
+    </div>
+  )
+}
